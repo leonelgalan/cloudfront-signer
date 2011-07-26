d27028e9738f507536735599f7ce49500223113d
Anthony Bouch <tony@58bits.com>
2011-07-26 03:25:50 -0400

    More refactoring of lib and added specs.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rspec'
+require 'aws_cf_signer'
+
+def get_query_value(url, key)
+    query_string = url.slice((url =~ /\?/) + 1..-1) 
+    pairs = query_string.split('&')
+    pairs.each do |item|
+        if item.start_with?(key)
+            return item.split('=')[1]
+        end
+    end
+end
+
+
+RSpec.configure do |config|
+
+end
